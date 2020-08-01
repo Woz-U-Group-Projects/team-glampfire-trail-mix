e919052fe34850aed7e6f6b36b91a36d4b2e4ac9
Kevin Isham <5745349-izaxone@users.noreply.gitlab.com>
2020-08-01 15:40:08 -0600

    Added SpringBootServletInitializer

--- server-java-mongodb/src/main/java/com/example/groupproject/GroupProjectApplication.java
+++ server-java-mongodb/src/main/java/com/example/groupproject/GroupProjectApplication.java
@@ -2,12 +2,14 @@ package com.example.groupproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class GroupProjectApplication {
+// SpringBootServletInitializer is required for Tomcat to treat this as a Spring Boot application
+public class GroupProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroupProjectApplication.class, args);
