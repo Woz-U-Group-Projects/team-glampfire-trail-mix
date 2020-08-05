97836a3e391f15aa31488a48fe05b436fe0aa220
Andrew Strain <andrew.d.strain@outlook.com>
2020-08-05 11:19:32 -0400

    Change POST to return RESTful message

--- server-java-mongodb/src/main/java/com/example/groupproject/controllers/ImageController.java
+++ server-java-mongodb/src/main/java/com/example/groupproject/controllers/ImageController.java
@@ -1,30 +1,25 @@
 package com.example.groupproject.controllers;
 
 import com.example.groupproject.models.Image;
-import com.example.groupproject.controllers.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Controller
+@RestController
 public class ImageController {
 
     @Autowired
     private ImageService imageService;
 
     @PostMapping("/images/add")
-    public String addImage(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<String> addImage(@RequestParam("file") MultipartFile file) throws IOException {
         String id = imageService.addImage(file);
-        return "redirect:/images/" + id;
+        return ResponseEntity.ok("/images/" + id);
     }
 
     @GetMapping("/images/{id}")
