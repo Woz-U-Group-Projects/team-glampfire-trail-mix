47c038ebae8bc070fed86fa44775ca1ddcd4fbff
Andrew Strain <andrew.d.strain@outlook.com>
2020-08-15 00:39:52 -0400

    Shorten request mapping

--- server-java-mongodb/src/main/java/com/example/groupproject/controllers/ProfileInformationController.java
+++ server-java-mongodb/src/main/java/com/example/groupproject/controllers/ProfileInformationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.groupproject.models.ProfileInformationRepository;
 
 @RestController
-@RequestMapping("/ProfileInformation")
+@RequestMapping("/profileinfo")
 public class ProfileInformationController {
 
     @Autowired
