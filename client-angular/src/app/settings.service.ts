f7af03c93b34e77cba60c33c8ec0a9ad6a1b3868
Andrew Strain <andrew.d.strain@outlook.com>
2020-08-18 13:20:52 -0400

    Fix localhost base URL.

--- client-angular/src/app/settings.service.ts
+++ client-angular/src/app/settings.service.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 export class SettingsService {
   // BASE_URL = 'http://52.156.70.167:8080/glampfire/';
   // BASE_URL = 'http://52.156.70.167:8080/glampfiredev/';
-  BASE_URL = 'http://52.156.70.167:8080/glampfiredev/';
+  BASE_URL = 'http://localhost:8080/';
   apiUrl = this.BASE_URL + 'settings';
 
   constructor(private http: HttpClient) { }
