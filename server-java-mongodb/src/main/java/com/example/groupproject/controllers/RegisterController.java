aaf8bcb5bca8ae0d8b27d5af6e437f3f740687ee
Andrew Strain <andrew.d.strain@outlook.com>
2020-08-27 08:38:48 -0400

    Delete unused models. Rename HomeController.

--- server-java-mongodb/src/main/java/com/example/groupproject/controllers/RegisterController.java
+++ server-java-mongodb/src/main/java/com/example/groupproject/controllers/RegisterController.java
@@ -3,26 +3,19 @@ package com.example.groupproject.controllers;
 import com.example.groupproject.models.User;
 import com.example.groupproject.models.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
-        @Autowired
-        private UsersRepository userRepository;
+    private final UsersRepository userRepository;
 
-    @GetMapping("/")
-    public String getHomePage() {
-        return "home";
+    @Autowired
+    public HomeController(UsersRepository userRepository) {
+        this.userRepository = userRepository;
     }
-
-
-
-
-
 
     @PostMapping("/register")
     public String createUser(@RequestParam("username") String username, @RequestParam("password") String password, Model model) {
@@ -33,8 +26,7 @@ public class HomeController {
             newUser.setPassword(password);
             userRepository.save(newUser);
             return "login";
-        }
-        else {
+        } else {
             model.addAttribute("exists", true);
             return "register";
         }
