1bd6e3b3b9c54e54343d4404a73cd0fb005e7c07
Andrew Strain <andrew.d.strain@outlook.com>
2020-08-31 15:13:44 -0400

    Make User model match what is on the server

--- client-angular/src/app/models/User.ts
+++ client-angular/src/app/models/User.ts
@@ -1,10 +1,6 @@
 export class User{
     id: string;
-    firstName: string;
-    lastName: string;
-    emailAddress: string;
     userName: string;
     password: string;
-    socialMedia: string;
-    
+    role: string;    
 }
