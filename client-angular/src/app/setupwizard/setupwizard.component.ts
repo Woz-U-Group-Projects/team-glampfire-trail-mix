a7373192000cd8ed66669e1cb503ed34ba9fddae
Kevin Isham <kevin@izaxone.com>
2020-08-31 16:58:09 -0600

    Correct name of User model

--- client-angular/src/app/setupwizard/setupwizard.component.ts
+++ client-angular/src/app/setupwizard/setupwizard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../models/user';
+import { User } from '../models/User';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../user.service';
