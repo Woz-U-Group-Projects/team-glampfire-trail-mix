7d7c1dd8ac42f822ab36ae0ae8e7633929c0387d
Andrew Strain <andrew.d.strain@outlook.com>
2020-09-03 13:54:34 -0400

    Dynamically change web site tite, favicon, and theme.

--- client-angular/src/app/admin/settings/settings.component.ts
+++ client-angular/src/app/admin/settings/settings.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SettingsService } from '@app/services/settings.service';
 import { Settings } from '@app/models/settings';
+import { AppComponent } from '@app/app.component';
 
 @Component({
   selector: 'app-settings',
@@ -12,7 +13,7 @@ export class SettingsComponent implements OnInit {
 
   settings: Settings;
 
-  constructor(private service: SettingsService, private router: Router) { }
+  constructor(private service: SettingsService, private router: Router, private app: AppComponent) { }
 
   getSettings() {
     this.service.getSettings().subscribe(settings => {
@@ -29,6 +30,9 @@ export class SettingsComponent implements OnInit {
     this.service.putSettings(this.settings).subscribe(settings => {
       this.settings = settings;
       alert('The settings are saved.');
+      this.app.changeTheme(this.settings.theme);
+      this.app.changeTitle(this.settings.headTitle);
+      this.app.changeFavicon(this.settings.favicon);
       this.router.navigateByUrl('/admin').then();
     });
   }
