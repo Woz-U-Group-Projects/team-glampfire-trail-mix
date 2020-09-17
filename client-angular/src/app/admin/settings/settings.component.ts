b2653007474be00e7e635df8243fe55f1c71fd00
Andrew Strain <andrew.d.strain@outlook.com>
2020-09-17 01:32:48 -0400

    Dropdown theme chooser

--- client-angular/src/app/admin/settings/settings.component.ts
+++ client-angular/src/app/admin/settings/settings.component.ts
@@ -1,48 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { SettingsService } from '@app/services/settings.service';
-import { Settings } from '@app/models/settings';
-import { AppComponent } from '@app/app.component';
-import { environment } from '@environments/environment';
+import {Component, Input, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {SettingsService} from '@app/services/settings.service';
+import {Settings} from '@app/models/settings';
+import {AppComponent} from '@app/app.component';
+import {environment} from '@environments/environment';
+import {Theme, Themes} from '@app/models/theme';
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+    selector: 'app-settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
 
-  settings: Settings = new Settings();
+    settings: Settings = new Settings();
+    themes: Theme[] = Themes;
 
-  constructor(private service: SettingsService, private router: Router, private app: AppComponent) { }
+    constructor(private service: SettingsService, private router: Router, private app: AppComponent) {
+    }
 
-  getSettings() {
-    this.service.getSettings().subscribe(settings => {
-      this.settings = settings;
-    });
-  }
+    getSettings() {
+        this.service.getSettings().subscribe(settings => {
+            this.settings = settings;
+        });
+    }
 
 
-  ngOnInit() {
-    this.getSettings();
-    this.app.loadExternalStyles('FontAwesome', environment.fontawesomeUrl).then();
-  }
+    ngOnInit() {
+        this.getSettings();
+        this.app.loadExternalStyles('FontAwesome', environment.fontawesomeUrl).then();
+    }
 
-  onSubmit() {
-    this.service.putSettings(this.settings).subscribe(settings => {
-      this.settings = settings;
-      alert('The settings are saved.');
-      this.app.loadExternalStyles('theme', this.settings.theme, true).then(() => {
-        console.log('Theme reloaded');
-      });
-      this.app.changeTitle(this.settings.headTitle).then(() => {
-        console.log('Web page title reloaded');
-      });
-      this.app.changeFavicon(this.settings.favicon).then(() => {
-        console.log('Favicon reloaded');
-      });
-      this.router.navigateByUrl('/admin/posts').then();
-    });
-  }
+    onSubmit() {
+        this.service.putSettings(this.settings).subscribe(settings => {
+            this.settings = settings;
+            alert('The settings are saved.');
+            this.app.loadExternalStyles('theme', this.settings.theme, true).then(() => {
+                console.log('Theme reloaded');
+            });
+            this.app.changeTitle(this.settings.headTitle).then(() => {
+                console.log('Web page title reloaded');
+            });
+            this.app.changeFavicon(this.settings.favicon).then(() => {
+                console.log('Favicon reloaded');
+            });
+            this.router.navigateByUrl('/admin/posts').then();
+        });
+    }
+
+    changeThemeValue() {
+        const uri = document.forms[0].themeValue.value;
+        document.forms[0].theme.value = uri;
+        this.settings.theme = uri;
+    }
 
 }
---
+++ client-angular/src/app/models/theme.ts
@@ -0,0 +1,29 @@
+export class Theme {
+    name: string;
+    url: string;
+}
+
+export const Themes: Theme[] = [
+    {name: 'Amber', url: 'https://www.w3schools.com/lib/w3-theme-amber.css'},
+    {name: 'Black', url: 'https://www.w3schools.com/lib/w3-theme-black.css'},
+    {name: 'Blue', url: 'https://www.w3schools.com/lib/w3-theme-blue.css'},
+    {name: 'Blue Grey', url: 'https://www.w3schools.com/lib/w3-theme-blue-grey.css'},
+    {name: 'Brown', url: 'https://www.w3schools.com/lib/w3-theme-brown.css'},
+    {name: 'Cyan', url: 'https://www.w3schools.com/lib/w3-theme-cyan.css'},
+    {name: 'Dark Grey', url: 'https://www.w3schools.com/lib/w3-theme-dark-grey.css'},
+    {name: 'Deep Orange', url: 'https://www.w3schools.com/lib/w3-theme-deep-orange.css'},
+    {name: 'Deep Purple', url: 'https://www.w3schools.com/lib/w3-theme-deep-purple.css'},
+    {name: 'Green', url: 'https://www.w3schools.com/lib/w3-theme-green.css'},
+    {name: 'Grey', url: 'https://www.w3schools.com/lib/w3-theme-grey.css'},
+    {name: 'Indigo', url: 'https://www.w3schools.com/lib/w3-theme-indigo.css'},
+    {name: 'Khaki', url: 'https://www.w3schools.com/lib/w3-theme-khaki.css'},
+    {name: 'Light Blue', url: 'https://www.w3schools.com/lib/w3-theme-light-blue.css'},
+    {name: 'Light Green', url: 'https://www.w3schools.com/lib/w3-theme-light-green.css'},
+    {name: 'Lime', url: 'https://www.w3schools.com/lib/w3-theme-lime.css'},
+    {name: 'Orange', url: 'https://www.w3schools.com/lib/w3-theme-orange.css'},
+    {name: 'Pink', url: 'https://www.w3schools.com/lib/w3-theme-pink.css'},
+    {name: 'Purple', url: 'https://www.w3schools.com/lib/w3-theme-purple.css'},
+    {name: 'Red', url: 'https://www.w3schools.com/lib/w3-theme-red.css'},
+    {name: 'Teal', url: 'https://www.w3schools.com/lib/w3-theme-teal.css'},
+    {name: 'Yellow', url: 'https://www.w3schools.com/lib/w3-theme-yellow.css'}
+];
