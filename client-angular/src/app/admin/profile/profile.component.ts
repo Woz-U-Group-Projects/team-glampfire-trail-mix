142470ef9fe3c700b048bc24ce3fea2b325810cf
Andrew Strain <andrew.d.strain@outlook.com>
2020-09-18 06:41:30 -0400

    Make About tab dynamic based on fields used

--- client-angular/src/app/admin/profile/profile.component.ts
+++ client-angular/src/app/admin/profile/profile.component.ts
@@ -1,35 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ProfileService } from '@app/services/profile.service';
-import { Profile } from '@app/models/profile';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ProfileService} from '@app/services/profile.service';
+import {Profile} from '@app/models/profile';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
-  profile: Profile = new Profile();
-  languages = '';
+    profile: Profile = new Profile();
+    languages = '';
 
-  constructor(private router: Router, private service: ProfileService) { }
+    constructor(private router: Router, private service: ProfileService) {
+    }
 
-  ngOnInit() {
-    this.service.readProfile().subscribe(profile => {
-      if (profile) {
-        this.profile = profile;
-        this.languages = this.profile.languages.join(', ');
-      }
-    });
-  }
+    ngOnInit() {
+        this.service.readProfile().subscribe(profile => {
+            if (profile) {
+                this.profile = profile;
 
-  onSubmit() {
-    this.profile.languages = this.languages.split(/, */);
-    this.service.updateProfile(this.profile).subscribe(profile => {
-      alert('Saved profile');
-      this.router.navigateByUrl('/admin/posts').then();
-    });
-  }
+                if (this.profile.languages) {
+                    this.languages = this.profile.languages.join(', ');
+                }
+            }
+        });
+    }
+
+    onSubmit() {
+        this.profile.languages = this.languages.split(/, */);
+        if (this.profile.languages[0].length === 0) {
+            this.profile.languages = null;
+        }
+
+        this.service.updateProfile(this.profile).subscribe(() => {
+            alert('Saved profile');
+            this.router.navigateByUrl('/admin/posts').then();
+        });
+    }
 
 }
